5dc25635704ef8f103fb085a17aa7d1dd49c5447
Colleen Higgins <cjh323@cornell.edu>
2019-06-11 15:43:35 -0400

    completed feat 7 all items

--- client/routes.js
+++ client/routes.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
-import {me} from './store'
+import {Login, Signup, UserHome, AllItems} from './components'
+import {me, fetchItems} from './store'
 
 /**
  * COMPONENT
@@ -21,6 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/all" component={AllItems} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
@@ -49,6 +50,7 @@ const mapDispatch = dispatch => {
   return {
     loadInitialData() {
       dispatch(me())
+      dispatch(fetchItems())
     }
   }
 }
