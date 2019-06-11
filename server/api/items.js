ab026a1fba2ddb55304f87883ee386db26420080
Colleen Higgins <cjh323@cornell.edu>
2019-06-11 14:52:29 -0400

    feature 21 complete

--- server/api/items.js
+++ server/api/items.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const {Item} = require('../db/models')
 module.exports = router
-router.get('/', async (req, res, next) => {
+router.get('/all', async (req, res, next) => {
   try {
-    const items = await Item.findAll({})
+    const items = await Item.findAll()
     res.json(items)
   } catch (err) {
     next(err)
