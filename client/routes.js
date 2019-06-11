e2ba5b7533654c1f272c99b6f08f8861bc39494d
Taylor Thompson <ttaythompson@gmail.com>
2019-06-11 17:04:14 -0400

    completed feature 29

--- client/routes.js
+++ client/routes.js
@@ -19,10 +19,11 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/items/:id" component={SingleItem} />
+        <Route exact path="/items/all" component={AllItems} />
+        <Route exact path="/items/:id" component={SingleItem} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/items/all" component={AllItems} />
+
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
