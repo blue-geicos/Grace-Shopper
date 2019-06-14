a06f16e961fbc25d069a3b6dfbfc653c63982520
Colleen Higgins <cjh323@cornell.edu>
2019-06-14 14:58:29 -0400

    finished feat 47, so a user can see their active cart after logging out and logging back in

--- server/api/orders.js
+++ server/api/orders.js
@@ -6,8 +6,6 @@ module.exports = router
 router.put('/checkout', async (req, res, next) => {
   try {
     const orderToUpdate = await Order.findByPk(req.body.orderId)
-    console.log('req body', req.body)
-    console.log('order to update', orderToUpdate)
     const placedOrder = await orderToUpdate.update(
       {
         cartMode: false
@@ -50,20 +48,11 @@ router.post('/guest-checkout', async (req, res, next) => {
 
 router.put('/add-to-cart', async (req, res, next) => {
   try {
-    // const order = await Order.findOrCreate({
-    //   where: {
-    //     id: req.body.orderId
-    //   },
-    //   include: [{model: Item}],
-    //   plain: true
-    // })
     let order
     if (req.body.orderId) {
       order = await Order.findByPk(req.body.orderId, {include: [{model: Item}]})
-      console.log('order when orderid exists', order)
     } else {
       order = await Order.create()
-      console.log('order when order id DOES NOT exist', order)
     }
     const user = await User.findByPk(req.body.userId)
     await order.setUser(user)
@@ -93,8 +82,19 @@ router.put('/add-to-cart', async (req, res, next) => {
       const itemToAdd = await Item.findByPk(req.body.itemId)
       await order.addItem(itemToAdd)
     }
-
     res.json(order)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/cart', async (req, res, next) => {
+  try {
+    const orderData = await Order.findOne({
+      where: {userId: req.userId, cartMode: true}
+    })
+    const cart = await orderData.getOrderWithItemsAndQuantities()
+    res.json(cart)
   } catch (err) {
     next(err)
   }
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -13,9 +13,30 @@ OrderItems.addHook('afterUpdate', orderItemsInstance => {
   const item = Item.findByPk(orderItemsInstance.itemId)
   if (orderItemsInstance.quantity > item.stock) {
     orderItemsInstance.quantity = item.stock
-    return orderItemsInstance
   }
+  return orderItemsInstance
 })
+
+// Methods
+Order.prototype.getOrderWithItemsAndQuantities = async function() {
+  const orderId = this.id
+  const itemIdsQuants = await OrderItems.findAll({
+    where: {orderId: orderId},
+    attributes: ['quantity', 'itemId']
+  })
+
+  const itemsArray = itemIdsQuants.map(async orderItems => {
+    const itemQuant = orderItems.dataValues.quantity
+    const itemId = orderItems.dataValues.itemId
+    const itemObj = await Item.findByPk(itemId, {
+      attributes: ['id', 'name', 'description', 'price', 'imageUrl']
+    })
+    return {...itemObj.dataValues, quantity: itemQuant}
+  })
+  return Promise.all(itemsArray).then(cart => {
+    return {cart, orderId}
+  })
+}
 
 module.exports = {
   User,
