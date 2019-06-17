9df442c017b9c306afd25f374b488badd8096514
AudraKenney <audrakkenney@gmail.com>
2019-06-17 18:19:28 -0400

    CSS Styling

--- client/components/all-items.js
+++ client/components/all-items.js
@@ -6,70 +6,65 @@ import {Link} from 'react-router-dom'
 import {makeStyles} from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-
-import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles({
   card: {
-    width: 450
+    width: 450,
+    shadow: 0
   },
   media: {
     height: 325
   }
 })
 
-const useStylesGrid = makeStyles(theme => ({
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    gridGap: theme.spacing(5)
-  }
-}))
-
 export const AllItems = props => {
   const classes = useStyles()
-  const gridClasses = useStylesGrid()
 
   return (
-    <div id="all-items">
+    <div>
       <h3>All Items</h3>
       <br />
-      <Grid container spacing={5}>
+      <div id="all-items">
+        {/* <Grid container spacing={5}> */}
         {props.items.map(item => (
           <div key={item.id} className="all-view">
-            <Grid item className={gridClasses}>
-              <Link to={`/items/${item.id}`}>
-                <Card className={classes.card}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image={item.imageUrl}
-                      title={item.name}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {item.name}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        ${item.price / 100}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              </Link>
-            </Grid>
+            {/* <Grid item className={gridClasses}> */}
+            <Link to={`/items/${item.id}`}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media}
+                    image={item.imageUrl}
+                    title={item.name}
+                  />
+                  <CardContent>
+                    <Typography
+                      className="item-card-title"
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                    >
+                      {item.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      ${item.price / 100}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
+            {/* </Grid> */}
           </div>
         ))}
-      </Grid>
+        {/* </Grid> */}
+      </div>
     </div>
   )
 }
--- script/seed.js
+++ script/seed.js
@@ -54,12 +54,41 @@ async function seed() {
     }),
     Item.create({
       name: 'Congrats',
-      description: `Premium champagne is paired with gourmet confections, making an ideal gift to send your congratulations and best wishes. Served up in a fleur-de-lis keepsake container, it's an elegant gift that is sure to surprise, delight and leave a lasting impression. Included in the basket: Chocolate Chip Cookies, Almonds, Dunlin California Brut Sparkling Wine, Cherry Sours, Peanut Brittle, Moet & Chandon Imperial Champagne, Olive Oil and Sea Salt Crostini d'Italia, and Roasted Cinnamon Maple Walnuts`,
+      description: `Premium champagne is paired with gourmet confections, making an ideal gift to send your congratulations and best wishes. Served up in a fleur-de-lis keepsake container, it's an elegant gift that is sure to surprise, delight and leave a lasting impression. Included in the basket: Chocolate Chip Cookies, Almonds, Dunlin California Brut Sparkling Wine, Cherry Sours, Peanut Brittle, Moet & Chandon Imperial Champagne, Olive Oil and Sea Salt Crostini d'Italia, and Roasted Cinnamon Maple Walnuts
+
+      `,
       price: 7999,
       category: 'Special Occasions',
       imageUrl:
         'https://www.gifttree.com/images/super/5061bg_Champagne-Wishes-Gift-Basket.jpg',
       stock: 5
+    }),
+    Item.create({
+      name: `Treat Yo'self`,
+      description: 'Relax and self-care',
+      price: 3599,
+      category: 'Self Care',
+      imageUrl:
+        'https://www.gifttree.com/images/super/2143ai_Bath-and-Body-Invigoration.jpg',
+      stock: 10
+    }),
+    Item.create({
+      name: 'Welcome Home!',
+      description: 'Housewarming',
+      price: 5599,
+      category: 'Housewarming',
+      imageUrl:
+        'https://www.gifttree.com/images/gt_large/27262b_Welcome-Home-Coffee-For-Two.jpg',
+      stock: 10
+    }),
+    Item.create({
+      name: 'Netflix & Chill',
+      description: 'Date night inspiration',
+      price: 7599,
+      category: 'Date Night',
+      imageUrl:
+        'https://www.gifttree.com/images/super/6603bi_The-5th-Avenue-Wine-Gift-Basket.jpg',
+      stock: 10
     })
   ])
 
